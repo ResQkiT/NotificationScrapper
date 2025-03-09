82d512362846753d8efe742632f53dd201b6197f
Ilya Safronov <yaizkima@gmail.com>
2025-03-09 23:14:28 +0300

    i dont know how to name commit with a single line changed

--- scrapper/src/main/java/backend/academy/scrapper/DomainsConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/DomainsConfig.java
@@ -3,5 +3,4 @@ package backend.academy.scrapper;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "domains")
-public record DomainsConfig(String github, String stackoverflow, String bot, String telegramBotUrl) {
-}
+public record DomainsConfig(String github, String stackoverflow, String bot, String telegramBotUrl) {}
